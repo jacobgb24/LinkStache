cdca39abb0f58580c3035e580f401f60ff756930
jacobgb24 <jacobldavis98@gmail.com>
2022-11-02 23:01:49 -0700

    early work for customizing style

--- helper.js
+++ helper.js
@@ -2,5 +2,13 @@ document.addEventListener('DOMContentLoaded', () => {
     fetch('./data.json').then((response) => response.json()).then((json) => {     
         let body = document.getElementById("body");
         body.innerHTML = Mustache.render(body.innerHTML, json);
+
+        fetch('./style.css').then((response) => response.text()).then((body) => {
+            var style = document.createElement('style');
+            style.innerHTML = Mustache.render(body, json.style);
+            document.getElementsByTagName('head')[0].appendChild(style);
+        })
     });
+
+    
 });
