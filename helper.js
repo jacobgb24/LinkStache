dfba9f01e81fdd3c276d2e98c406bbc56544bece
jacobgb24 <jacobldavis98@gmail.com>
2022-11-14 22:52:41 -0800

    load html after style

--- helper.js
+++ helper.js
@@ -1,14 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
     fetch('./data.json').then((response) => response.json()).then((json) => {     
-        let template = document.getElementById("template").innerHTML;
-        document.getElementById("target").innerHTML = Mustache.render(template, json);
-
         document.title = json.profile.name
 
         fetch('./style.css').then((response) => response.text()).then((body) => {
             var style = document.createElement('style');
             style.innerHTML = Mustache.render(body, json.style);
             document.getElementsByTagName('head')[0].appendChild(style);
+
+            // load html after style to avoid jarring changes
+            let template = document.getElementById("template").innerHTML;
+            document.getElementById("target").innerHTML = Mustache.render(template, json);
         })
     });
 
